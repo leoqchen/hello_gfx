5fa49a58294228b77d0fce45ba7105f45ac8f887
cqq <leo.q.chen@hotmail.com>
2024-01-05 18:03:14 +0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,228 @@
+.PHONY: default all clean print
+default: all
+
+
+#########################################################
+# 确定机器平台
+#
+UNAME_M := $(shell uname -m)
+ifeq ($(UNAME_M),x86_64)
+  TARGET_ARCH_ABI := x86_64
+endif
+ifeq ($(UNAME_M),aarch64)
+  TARGET_ARCH_ABI := arm64-v8a
+endif
+$(warning TARGET_ARCH_ABI=$(TARGET_ARCH_ABI))
+
+
+#########################################################
+# 确定gcc版本
+#
+GCC_VERSION = $(shell gcc -dumpversion)
+#$(warning GCC_VERSION=$(GCC_VERSION))
+
+
+#########################################################
+# 确定-j数目
+#
+MAKE_PID := $(shell echo $$PPID)
+JOB_FLAG := $(filter -j%, $(subst -j ,-j,$(shell ps T | grep "^\s*$(MAKE_PID).*$(MAKE)")))
+JOBS     := $(subst -j,,$(JOB_FLAG))
+ifeq ($(JOBS),)
+	JOBS := 1
+endif
+#$(warning JOBS=$(JOBS))
+
+
+#########################################################
+CC  := gcc
+CXX	:= g++
+CXXFLAGS +=
+LDFLAGS +=
+LIBS += 
+AR := gcc-ar
+ARFLAGS := rcs
+glslangValidator := glslangValidator -Os
+
+ObjectDir := obj
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+	ObjectDir := obj/x86_64
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+	ObjectDir := obj/arm64-v8a
+endif
+
+# for -D
+CommonFlagsD :=
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+	CommonFlagsD += 
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+	CommonFlagsD += 
+endif
+CommonFlagsD += 
+
+# for -I, -isystem
+CommonFlagsI := 
+
+# for compiler
+O := -O0
+CommonFlagsC := $(O) -std=c++20 -pipe -MMD -g -ggdb3 -fPIC -pthread -Wall -Werror
+CommonLinkFlags := $(O) -pipe -MMD -g -ggdb3 -pthread -Wl,--no-undefined
+CXX_ONLY := -std=c++20 -std=gnu++11 -std=gnu++17
+C_ONLY := -std=gnu99
+
+AllTargets :=
+
+#######################################################################################################
+
+#-------------------------------------------------------
+# glad
+#-------------------------------------------------------
+CommonFlags_glad := \
+	$(CommonFlagsC) \
+	$(CommonFlagsD) \
+	$(CommonFlagsI) \
+	-Isrc/utils \
+
+SourceDir_glad := src/utils/glad
+Source_glad := \
+	glad.c
+SourceMain_glad :=
+
+CXXFLAGS_glad := \
+	$(CommonFlags_glad) \
+	$(patsubst %, -I%, $(SourceDir_glad)) \
+
+CFLAGS_glad := $(filter-out $(CXX_ONLY), $(CXXFLAGS_glad))  #remove c++ only flags
+
+LocalLib_glad :=
+ExtLib_glad := 
+#---------------------------------
+
+Object_glad := $(patsubst %.c, $(ObjectDir)/$(SourceDir_glad)/%.o, $(Source_glad))
+Object_glad := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_glad)/%.o, $(Object_glad))
+Object_glad := $(patsubst %.S, $(ObjectDir)/$(SourceDir_glad)/%.o, $(Object_glad))
+
+ObjectMain_glad := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_glad)/%.o, $(SourceMain_glad))
+BinaryMain_glad := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_glad)/%, $(SourceMain_glad))
+AR_glad := $(ObjectDir)/$(SourceDir_glad)/libglad.a
+
+Depand_glad := $(Object_glad:.o=.d) $(ObjectMain_glad:.o=.d)
+
+# *.o <-- *.c
+$(ObjectDir)/$(SourceDir_glad)/%.o: $(SourceDir_glad)/%.c
+	@printf "\n" && mkdir -p $(@D)
+	$(CC) $(CFLAGS_glad) -c $< -o $@
+
+# *.o <-- *.cpp
+$(ObjectDir)/$(SourceDir_glad)/%.o: $(SourceDir_glad)/%.cpp
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_glad) -c $< -o $@
+
+# *.o <-- *.S
+$(ObjectDir)/$(SourceDir_glad)/%.o: $(SourceDir_glad)/%.S
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_glad) -c $< -o $@
+
+# *.a <-- *.o
+$(AR_glad): $(Object_glad)
+	@printf "\n" && mkdir -p $(@D)
+	$(AR) $(ARFLAGS) $@ $^
+
+# binary <-- *.o *.a
+$(ObjectDir)/$(SourceDir_glad)/%: $(ObjectDir)/$(SourceDir_glad)/%.o $(Object_glad) $(LocalLib_glad)
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_glad) -Wl,--start-group $^ $(ExtLib_glad) -Wl,--end-group -o $@
+	@mkdir -p $(ObjectDir)/bin && ln -s -r -f $@ -t $(ObjectDir)/bin
+
+.PHONY: glad
+glad: $(Object_glad) $(AR_glad) $(ObjectMain_glad) $(BinaryMain_glad) 
+
+-include $(Depand_glad)
+AllTargets += glad
+
+
+#-------------------------------------------------------
+# gl
+#-------------------------------------------------------
+CommonFlags_gl := \
+	$(CommonFlagsC) \
+	$(CommonFlagsD) \
+	$(CommonFlagsI) \
+	-Isrc/utils \
+	-Isrc/gl \
+
+SourceDir_gl := src/gl
+Source_gl := 
+SourceMain_gl := \
+	hello_triangle.cpp \
+
+CXXFLAGS_gl := \
+	$(CommonFlags_gl) \
+	$(patsubst %, -I%, $(SourceDir_gl)) \
+
+CFLAGS_gl := $(filter-out $(CXX_ONLY), $(CXXFLAGS_gl))  #remove c++ only flags
+
+LocalLib_gl := ${AR_glad}
+ExtLib_gl := \
+	-ldl \
+	`pkg-config --libs glfw3` \
+
+#---------------------------------
+
+Object_gl := $(patsubst %.c, $(ObjectDir)/$(SourceDir_gl)/%.o, $(Source_gl))
+Object_gl := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_gl)/%.o, $(Object_gl))
+Object_gl := $(patsubst %.S, $(ObjectDir)/$(SourceDir_gl)/%.o, $(Object_gl))
+
+ObjectMain_gl := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_gl)/%.o, $(SourceMain_gl))
+BinaryMain_gl := $(patsubst %.cpp, $(ObjectDir)/$(SourceDir_gl)/%, $(SourceMain_gl))
+AR_gl := $(ObjectDir)/$(SourceDir_gl)/libgl.a
+
+Depand_gl := $(Object_gl:.o=.d) $(ObjectMain_gl:.o=.d)
+
+# *.o <-- *.c
+$(ObjectDir)/$(SourceDir_gl)/%.o: $(SourceDir_gl)/%.c
+	@printf "\n" && mkdir -p $(@D)
+	$(CC) $(CFLAGS_gl) -c $< -o $@
+
+# *.o <-- *.cpp
+$(ObjectDir)/$(SourceDir_gl)/%.o: $(SourceDir_gl)/%.cpp
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_gl) -c $< -o $@
+
+# *.o <-- *.S
+$(ObjectDir)/$(SourceDir_gl)/%.o: $(SourceDir_gl)/%.S
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_gl) -c $< -o $@
+
+# *.a <-- *.o
+$(AR_gl): $(Object_gl)
+	@printf "\n" && mkdir -p $(@D)
+	$(AR) $(ARFLAGS) $@ $^
+
+# binary <-- *.o *.a
+$(ObjectDir)/$(SourceDir_gl)/%: $(ObjectDir)/$(SourceDir_gl)/%.o $(Object_gl) $(LocalLib_gl)
+	@printf "\n" && mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS_gl) -Wl,--start-group $^ $(ExtLib_gl) -Wl,--end-group -o $@
+	@mkdir -p $(ObjectDir)/bin && ln -s -r -f $@ -t $(ObjectDir)/bin
+
+.PHONY: gl
+gl: $(Object_gl) $(AR_gl) $(ObjectMain_gl) $(BinaryMain_gl) 
+
+-include $(Depand_gl)
+AllTargets += gl
+
+
+#######################################################################################################
+all: $(AllTargets)
+
+# debug print
+print:
+	@printf "JOBS=$(JOBS)\n"
+	@printf "Object_XXX=$(Object_XXX)\n"
+	@printf "ObjectMain_XXX=$(ObjectMain_XXX)\n"
+
+clean:
+	rm -rf $(ObjectDir)/*
+
---
+++ src/gl/hello_triangle.cpp
@@ -0,0 +1,226 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+int main( int argc, const char* argv[] )
+{
+    if( argc != 2 ){
+        Usage:
+        printf("Usage: %s [gl|gles]\n", argv[0]);
+        return 1;
+    }
+
+    int isGLES = 0;
+    for( int i=1; i < argc; i++ ){
+        const char* arg = argv[i];
+        if( strcmp(arg, "gl") == 0 )
+            isGLES = 0;
+        else if( strcmp(arg, "gles") == 0 )
+            isGLES = 1;
+        else{
+            printf("invalid argument: %s\n", arg);
+            goto Usage;
+        }
+    }
+
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    if( isGLES ){
+        glfwWindowHint( GLFW_CLIENT_API, GLFW_OPENGL_ES_API );
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    }else {
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    }
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    printf("GL_VERSION = %s\n", glGetString(GL_VERSION));
+    int major_version = 0;
+    int minor_version = 0;
+    glGetIntegerv(GL_MAJOR_VERSION, &major_version);
+    glGetIntegerv(GL_MINOR_VERSION, &minor_version);
+    printf("GL_MAJOR_VERSION = %d\n", major_version);
+    printf("GL_MINOR_VERSION = %d\n", minor_version);
+    printf("GL_EXTENSIONS = %s\n", glGetString(GL_EXTENSIONS));
+    int num_extensions = 0;
+    glGetIntegerv(GL_NUM_EXTENSIONS, &num_extensions);
+    printf("GL_NUM_EXTENSIONS = %d\n", num_extensions);
+    for( int i=0; i < num_extensions; i++ ){
+        printf("%s ", glGetStringi(GL_EXTENSIONS, i));
+    }
+    printf("\n");
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    }; 
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
