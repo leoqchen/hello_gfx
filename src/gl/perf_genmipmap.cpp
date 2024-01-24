9e569fb4bb52bcff17cb9443944832972f4f4a3d
cqq <leo.q.chen@hotmail.com>
2024-01-24 15:50:22 +0800

    rewrite perf_genmipmap, perf_copytex, to support glLegacy, gl, gles

--- src/gl/perf_genmipmap.cpp
+++ src/gl/perf_genmipmap.cpp
@@ -1,23 +1,31 @@
-#include <glad/glad.h>
+#if IS_GlEs
+#include <glad/gles2.h>
+#else
+#include <glad/gl.h>
+#endif
+
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include <iostream>
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
-
+#include "glutils.h"
+#include "glfwutils.h"
 #include "myutils.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
 
 // settings
 const int WinWidth = 200;
 const int WinHeight = 200;
 
 static GLboolean DrawPoint = GL_TRUE;
-static GLuint VBO;
-static GLuint TexObj = 0;
+static GLuint vertex_array;
+static GLuint vertex_buffer;
+static GLuint textureId = 0;
 static GLint BaseLevel, MaxLevel;
+static GLuint program;
+static GLint samplerLoc;
 
 struct vertex
 {
@@ -30,6 +38,52 @@ static const struct vertex vertices[1] = {
 
 #define VOFFSET(F) ((void *) offsetof(struct vertex, F))
 
+
+#if IS_GlEs
+const char *vertexShaderSource =
+    "#version 320 es\n"
+    "layout (location = 0) in vec2 vPos;\n"
+    "layout (location = 1) in vec2 vTexCoord;\n"
+    "out vec2 v_texCoord;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4( vPos.x, vPos.y, 0.0f, 1.0f );\n"
+    "   v_texCoord = vTexCoord;\n"
+    "}\n\0";
+
+const char *fragmentShaderSource =
+    "#version 320 es\n"
+    "precision mediump float;\n"
+    "in vec2 v_texCoord;\n"
+    "layout (location = 0) out vec4 outColor;\n"
+    "uniform sampler2D s_texture;\n"
+    "void main()\n"
+    "{\n"
+    "   outColor = texture( s_texture, v_texCoord );\n"
+    "}\n\0";
+#else
+const char *vertexShaderSource =
+    "#version 400\n"
+    "layout (location = 0) in vec2 vPos;\n"
+    "layout (location = 1) in vec2 vTexCoord;\n"
+    "out vec2 v_texCoord;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4( vPos.x, vPos.y, 0.0f, 1.0f );\n"
+    "   v_texCoord = vTexCoord;\n"
+    "}\n\0";
+
+const char *fragmentShaderSource =
+    "#version 400\n"
+    "in vec2 v_texCoord;\n"
+    "layout (location = 0) out vec4 outColor;\n"
+    "uniform sampler2D s_texture;\n"
+    "void main()\n"
+    "{\n"
+    "   outColor = texture( s_texture, v_texCoord );\n"
+    "}\n\0";
+#endif
+
 static void PerfInit(void)
 {
     //if( !glfwExtensionSupported("GL_ARB_framebuffer_object") ){
@@ -37,20 +91,61 @@ static void PerfInit(void)
     //    exit(1);
     //}
 
-    /* setup VBO w/ vertex data */
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexPointer(2, GL_FLOAT, sizeof(struct vertex), VOFFSET(x)); //TODO:FIXME:GLES, only GL2.1
-    glTexCoordPointer(2, GL_FLOAT, sizeof(struct vertex), VOFFSET(s));//TODO:FIXME:GLES, only GL2.1
-    glEnableClientState(GL_VERTEX_ARRAY);//TODO:FIXME:GLES, only GL2.1
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);//TODO:FIXME:GLES, only GL2.1
+    // build and compile our shader program
+    // ------------------------------------
+#if !IS_GlLegacy
+    program = CreateProgramFromSource( vertexShaderSource, fragmentShaderSource );
+    samplerLoc = glGetUniformLocation( program, "s_texture" );
+#endif
 
-    glGenTextures(1, &TexObj);
-    glBindTexture(GL_TEXTURE_2D, TexObj);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+#if IS_GlLegacy
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexPointer(2, GL_FLOAT, sizeof(struct vertex), VOFFSET(x));
+    glTexCoordPointer(2, GL_FLOAT, sizeof(struct vertex), VOFFSET(s));
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+#else
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const GLint vPos_location = glGetAttribLocation(program, "vPos");
+    const GLint vTexCoord_location = glGetAttribLocation(program, "vTexCoord");
+    printf("Attrib location: vPos=%d\n", vPos_location);
+    printf("Attrib location: vTexCoord=%d\n", vTexCoord_location);
+    glVertexAttribPointer(vPos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(struct vertex), (void*)VOFFSET(x));
+    glVertexAttribPointer(vTexCoord_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(struct vertex), (void *)VOFFSET(s) );
+    glEnableVertexAttribArray(vPos_location);
+    glEnableVertexAttribArray(vTexCoord_location);
+#endif
+
+    // set up texture data and configure texture attributes
+    // ------------------------------------------------------------------
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+#if IS_GlLegacy
     glEnable(GL_TEXTURE_2D);
+#else
+    glActiveTexture( GL_TEXTURE0 );
+    glBindTexture( GL_TEXTURE_2D, textureId );
+
+    glUseProgram(program);
+    // set the sampler texture unit to 0
+    glUniform1i( samplerLoc, 0 );
+#endif
 }
 
 static void GenMipmap(unsigned count)
@@ -92,7 +187,6 @@ static void PerfDraw(void)
 
         /* loop over max level */
         for (MaxLevel = NumLevels; MaxLevel > BaseLevel; MaxLevel--) {
-
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, BaseLevel);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, MaxLevel);
 
@@ -108,76 +202,31 @@ static void PerfDraw(void)
 
 int main( int argc, const char* argv[] )
 {
-    if( argc != 2 ){
-        Usage:
-        printf("Usage: %s [glXX|glesXX]\n", argv[0]);
-        return 1;
-    }
-
-    api_t api = parse_api( argv[1] );
-    if( api.api == -1 ){
-        printf("invalid argument: %s\n", argv[1]);
-        goto Usage;
-    }
+#if IS_GlEs
+    api_t api = {.api = API_GLES, .major = 3, .minor = 2};
+#elif IS_GlLegacy
+    api_t api = {.api = API_GLLegacy, .major = 3, .minor = 0};
+#else
+    api_t api = {.api = API_GL, .major = 3, .minor = 3};
+#endif
+    if( argc >= 2 && isdigit(argv[1][0]) )
+        api.major = argv[1][0] - '0';
+    if( argc >= 3 && isdigit(argv[2][0]) )
+        api.minor = argv[2][0] - '0';
+    printf("command line: major = %d, minor = %d\n", api.major, api.minor);
 
     // glfw: initialize and configure
     // ------------------------------
-    glfwInit();
-    if( api.api == 1 ){
-        glfwWindowHint( GLFW_CLIENT_API, GLFW_OPENGL_ES_API );
-    }else {
-        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, api.major);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, api.minor);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(WinWidth, WinHeight, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    printf("GL_VERSION = %s\n", glGetString(GL_VERSION));
-    int major_version = 0;
-    int minor_version = 0;
-    glGetIntegerv(GL_MAJOR_VERSION, &major_version);
-    glGetIntegerv(GL_MINOR_VERSION, &minor_version);
-    printf("GL_MAJOR_VERSION = %d\n", major_version);
-    printf("GL_MINOR_VERSION = %d\n", minor_version);
-
+    GLFWwindow* window = glfwInit_CreateWindow( api, WinWidth, WinHeight );
 
     // init
     // -----------
     PerfInit();
 
-
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
-        processInput(window);
-
         // render
         // ------
         PerfDraw();
@@ -188,25 +237,12 @@ int main( int argc, const char* argv[] )
         glfwPollEvents();
     }
 
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+
+
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
