3a3466f1325c9a665995344ccd632d07a2774fd1
cqq <leo.q.chen@hotmail.com>
2024-02-21 14:13:23 +0800

    add Bug reproduce test: gl_PointSize, for IMG gpu

---
+++ src/gl/gl_PointSize.cpp
@@ -0,0 +1,150 @@
+/**
+ * Bug复现：IMG需要在vertex shader里显式地设置gl_PointSize，才能让glDraw( GL_POINTS )正确的显示绘制结果
+ */
+#if IS_GlEs
+#include <glad/gles2.h>
+#else
+#include <glad/gl.h>
+#endif
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <ctype.h>
+#include "glutils.h"
+#include "glfwutils.h"
+#include "myutils.h"
+
+
+// settings
+static const int WinWidth = 200;
+static const int WinHeight = 200;
+static GLFWwindow* window;
+
+static GLuint VAO;
+static GLuint VBO;
+static GLuint program;
+
+struct vertex
+{
+    GLfloat x, y;
+};
+
+static const struct vertex vertices[4] = {
+    { -0.5, -0.5 },
+    {  0.5, -0.5 },
+    {  0.5,  0.5 },
+    { -0.5,  0.5 }
+};
+
+
+const char *vertexShaderSource =
+#if IS_GlEs
+    "#version 320 es\n"
+#else
+    "#version 400\n"
+#endif
+    "layout (location = 0) in vec2 vPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4( vPos.x, vPos.y, 0.0f, 1.0f );\n"
+//    "   gl_PointSize = 1.0;\n" //TODO:FIXME: IMG need this code in OpenGLES
+    "}\n\0";
+
+const char *fragmentShaderSource =
+#if IS_GlEs
+    "#version 320 es\n"
+    "precision mediump float;\n"
+#else
+    "#version 400\n"
+#endif
+    "layout (location = 0) out vec4 outColor;\n"
+    "void main()\n"
+    "{\n"
+    "   outColor = vec4( 1.0f, 1.0f, 1.0f, 1.0f );\n"
+    "}\n\0";
+
+static void Init()
+{
+    // build and compile our shader program
+    // ------------------------------------
+#if !IS_GlLegacy
+    program = CreateProgramFromSource( vertexShaderSource, fragmentShaderSource );
+    glUseProgram(program);
+#endif
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+#if IS_GlLegacy
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexPointer(2, GL_FLOAT, sizeof(struct vertex), (void *)0);
+    glEnableClientState(GL_VERTEX_ARRAY);
+#else
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const GLint vPos_location = glGetAttribLocation(program, "vPos");
+    printf("Attrib location: vPos=%d\n", vPos_location);
+    glVertexAttribPointer(vPos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(struct vertex), (void*) 0);
+    glEnableVertexAttribArray(vPos_location);
+#endif
+
+    // misc GL state
+    // ------------------------------------------------------------------
+}
+
+int main( int argc, const char* argv[] )
+{
+#if IS_GlEs
+    api_t api = {.api = API_GLES, .major = 3, .minor = 2};
+#elif IS_GlLegacy
+    api_t api = {.api = API_GLLegacy, .major = 3, .minor = 0};
+#else
+    api_t api = {.api = API_GL, .major = 3, .minor = 3};
+#endif
+    if( argc >= 2 && isdigit(argv[1][0]) )
+        api.major = argv[1][0] - '0';
+    if( argc >= 3 && isdigit(argv[2][0]) )
+        api.minor = argv[2][0] - '0';
+    printf("command line: major = %d, minor = %d\n", api.major, api.minor);
+
+    // glfw: initialize and configure
+    // ------------------------------
+    window = glfwInit_CreateWindow( api, WinWidth, WinHeight );
+
+    // init
+    // -----------
+    Init();
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
+        // ------
+        glDrawArrays(GL_POINTS, 0, 4);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
