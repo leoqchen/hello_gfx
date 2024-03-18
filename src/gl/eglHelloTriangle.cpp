a767c972449adb8425ebdd2ccc416a7d3961f3d9
cqq <leo.q.chen@hotmail.com>
2024-03-18 22:28:23 +0800

    EGL demo use glutils

--- src/gl/eglHelloTriangle.cpp
+++ src/gl/eglHelloTriangle.cpp
@@ -1,8 +1,3 @@
-//#if IS_GlEs
-//#include <GLES3/gl32.h>
-//#else
-//#include <GL/gl.h>
-//#endif
 #if IS_GlEs
 #include <glad/gles2.h>
 #else
@@ -13,152 +8,12 @@
 #include "stb_image_write.h"
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-//#include "glutils.h" //TODO
+#include "glutils.h"
 #include "myutils.h"
 #include "x11utils.h"
 #include "eglutils.h"
 
-//----------------------- //TODO
-#define glErrorCheck() \
-    {\
-      GLenum error;\
-      if( (error = glGetError()) != GL_NO_ERROR ){\
-        printf("glGetError: %s, %s:%s:%d\n", glErrorName(error), __func__, __FILE__, __LINE__);\
-        exit(EXIT_FAILURE);\
-      }\
-    }
-
-const char* glErrorName( GLenum error )
-{
-    switch( error ){
-        case GL_NO_ERROR:
-            return "GL_NO_ERROR";
-        case GL_INVALID_ENUM:
-            return "GL_INVALID_ENUM";
-        case GL_INVALID_VALUE:
-            return "GL_INVALID_VALUE";
-        case GL_INVALID_OPERATION:
-            return "GL_INVALID_OPERATION";
-        case GL_INVALID_FRAMEBUFFER_OPERATION:
-            return "GL_INVALID_FRAMEBUFFER_OPERATION";
-        case GL_OUT_OF_MEMORY:
-            return "GL_OUT_OF_MEMORY";
-        case GL_STACK_UNDERFLOW:
-            return "GL_STACK_UNDERFLOW";
-        case GL_STACK_OVERFLOW:
-            return "GL_STACK_OVERFLOW";
-        default:
-            return "";
-    }
-}
-
-const char* glContextFlagName( GLint flag )
-{
-    switch( flag ){
-#if !IS_GlEs
-        case GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT:
-            return "GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT";
-        case GL_CONTEXT_FLAG_NO_ERROR_BIT:
-            return "GL_CONTEXT_FLAG_NO_ERROR_BIT";
-#endif
-        case GL_CONTEXT_FLAG_DEBUG_BIT:
-            return "GL_CONTEXT_FLAG_DEBUG_BIT";
-        case GL_CONTEXT_FLAG_ROBUST_ACCESS_BIT:
-            return "GL_CONTEXT_FLAG_ROBUST_ACCESS_BIT";
-        default:
-            return "";
-    }
-}
-
-static const char* glShaderTypeName( GLenum shaderType )
-{
-    switch( shaderType ){
-        case GL_COMPUTE_SHADER:
-            return "GL_COMPUTE_SHADER";
-        case GL_VERTEX_SHADER:
-            return "GL_VERTEX_SHADER";
-        case GL_TESS_CONTROL_SHADER:
-            return "GL_TESS_CONTROL_SHADER";
-        case GL_TESS_EVALUATION_SHADER:
-            return "GL_TESS_EVALUATION_SHADER";
-        case GL_GEOMETRY_SHADER:
-            return "GL_GEOMETRY_SHADER";
-        case GL_FRAGMENT_SHADER:
-            return "GL_FRAGMENT_SHADER";
-        default:
-            return "";
-    }
-}
-
-static GLuint CreateShaderFromSource( GLenum type, const char *shaderSource )
-{
-    GLuint shader = glCreateShader( type );
-    glShaderSource( shader, 1, &shaderSource, NULL );
-    glCompileShader( shader );
-
-    // Check the compile status
-    GLint success;
-    glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
-    if( !success ){
-        GLint infoLen = 0;
-        glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &infoLen );
-        if( infoLen > 0 ){
-            char *infoLog = (char*)malloc( sizeof(char) * infoLen );
-            glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
-            printf("ERROR: shader compile failed, shaderType = %s\n%s\n", glShaderTypeName(type), infoLog);
-            free( infoLog );
-        }
-
-        glDeleteShader( shader );
-        exit(EXIT_FAILURE);
-        //return 0;
-    }
-
-    return shader;
-}
-
-static GLuint CreateProgramFromShader( GLuint vertShader, GLuint fragShader )
-{
-    GLuint program = glCreateProgram();
-    glAttachShader( program, vertShader );
-    glAttachShader( program, fragShader );
-    glLinkProgram ( program );
-
-    // Check the link status
-    GLint success;
-    glGetProgramiv ( program, GL_LINK_STATUS, &success );
-    if( !success ){
-        GLint infoLen = 0;
-        glGetProgramiv ( program, GL_INFO_LOG_LENGTH, &infoLen );
-        if( infoLen > 0 ){
-            char *infoLog = (char*)malloc ( sizeof ( char ) * infoLen );
-            glGetProgramInfoLog ( program, infoLen, NULL, infoLog );
-            printf("ERROR: programe link failed\n%s\n", infoLog);
-            free ( infoLog );
-        }
-
-        glDeleteProgram ( program );
-        exit(EXIT_FAILURE);
-        //return 0;
-    }
-
-    return program;
-}
-
-static GLuint CreateProgramFromSource( const char *vertShaderSource, const char *fragShaderSource )
-{
-    GLuint vertShader = CreateShaderFromSource( GL_VERTEX_SHADER, vertShaderSource );
-    GLuint fragShader = CreateShaderFromSource( GL_FRAGMENT_SHADER, fragShaderSource );
-    GLuint program = CreateProgramFromShader( vertShader, fragShader );
-
-    glDeleteShader( vertShader );
-    glDeleteShader( fragShader );
-
-    return program;
-}
-//-------------------
 
 // settings
 static const int WinWidth = 800;
